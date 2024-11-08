c8b5512135b11b28209e74fcbc4911ca2df84fa2
habibb2r <habibhk127@gmail.com>
2024-11-08 21:29:17 +0600

    Updated

--- src/Problem-8.ts
+++ src/Problem-8.ts
@@ -1,14 +1,13 @@
 {
 
-
-
     const validateKeys = <T>(obj: T, keys: (keyof T)[]): boolean => {
         const checkKeys : boolean = keys.every(key => obj[key])
         return checkKeys
       };
       
       const person = { name: "Alice", age: 25, email: "alice@example.com" };
-      console.log(validateKeys(person, ["name", "age"])); 
-    //   console.log(validateKeys(person, ["name", "age", "wrongkey"])); 
+
+      
+      console.log(validateKeys(person, ["name", "age", "wrongkey"])); 
 }
   
